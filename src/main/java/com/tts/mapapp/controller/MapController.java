16b613d6a571fd99eb9b35dc3e63bddd7125e512
Ricardo P <ricardopaucar1363@gmail.com>
2020-11-09 09:07:40 -0600

    added home mapping

--- src/main/java/com/tts/mapapp/controller/MapController.java
+++ src/main/java/com/tts/mapapp/controller/MapController.java
@@ -1,8 +1,15 @@
 package com.tts.mapapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MapController {
     
+    @GetMapping("/home")
+    public String getDefaultMap() {
+        return "index.html";
+    }
+
+
 }
