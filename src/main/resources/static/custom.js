edafd47d9aff609cdcdbe1988779ce3486adf1a0
Ricardo P <ricardopaucar1363@gmail.com>
2020-11-10 08:05:56 -0600

    added custom

--- src/main/resources/static/custom.js
+++ src/main/resources/static/custom.js
@@ -1,7 +1,32 @@
+
+//let coords = {lat: 39.9612, lng: -82.9988};
 let map;
+
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 39.9612, lng: -82.9988},
-        zoom: 10
+        center: coords,
+        zoom: 10,
+        scrollwheel: false,
+
+    });
+
+    var image = {url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png", 
+    scaledSize: new google.maps.Size(50, 50)};
+
+    let marker = new google.maps.Marker({
+        position: coords,
+        map:map,
+        icon: image,
+        animation: google.maps.Animation.DROP
+    });
+
+    let contentString = '<h2>' + city + ', ' + state + '</h2>';
+
+    let infowindow = new google.maps.InfoWindow({
+    content: contentString
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map,marker);
     });
 }
